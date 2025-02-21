0b87e1b29233a08967b4277e505ed767a3aefcc6
arasopraza <aras.opraza@gmail.com>
2025-02-21 14:20:11 +0700

    refactor: remove unused django app

--- movie_go_be/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from . import models
-
-# Register your models here.
-admin.site.register(models.User, UserAdmin)
--- movie_go_be/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MovieGoBeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movie_go_be'
