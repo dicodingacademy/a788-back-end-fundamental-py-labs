d35f79ca6c5337f84b72f32a0c7fc004dfe95bba
arasopraza <aras.opraza@gmail.com>
2025-06-03 13:07:17 +0700

    refactor: ada reverse match url

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('movies/', views.MovieListCreateView.as_view()),
-  path('movies/<uuid:pk>/', views.MovieDetailView.as_view()),
+  path('movies/', views.MovieListCreateView.as_view(), name='movie-list'),
+  path('movies/<uuid:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
 ]
