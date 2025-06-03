a642bb6d8b0fb0728fb355139b144b4747c772c9
arasopraza <aras.opraza@gmail.com>
2025-06-03 13:07:17 +0700

    refactor: ada reverse match url

--- seats/urls.py
+++ seats/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('seats/', views.SeatListCreateView.as_view()),
-  path('seats/<uuid:pk>/', views.SeatDetailView.as_view()),
+  path('seats/', views.SeatListCreateView.as_view(), name='seat-list'),
+  path('seats/<uuid:pk>/', views.SeatDetailView.as_view(), name='seat-detail'),
 ]
