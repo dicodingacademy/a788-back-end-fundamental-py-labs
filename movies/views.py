7d809b5d86429c02e2bd748d4b7379e9412a0d1a
arasopraza <aras.opraza@gmail.com>
2025-06-03 20:04:32 +0700

    refactor: add get object for detail view

--- movies/views.py
+++ movies/views.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.http import Http404
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.shortcuts import get_object_or_404
 from core.permissions import IsAdminOrSuperUser
 from .models import Movie
 from .serializers import MovieSerializer
@@ -30,6 +30,14 @@ class MovieListCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class MovieDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+            self.check_object_permissions(self.request, movie)
+            return movie
+        except Movie.DoesNotExist:
+            raise Http404
+
     authentication_classes = [JWTAuthentication]
 
     def get_permissions(self):
@@ -38,12 +46,12 @@ class MovieDetailView(APIView):
         return [IsAuthenticated()]
 
     def get(self, request, pk):
-        movie = get_object_or_404(Movie, pk=pk)
+        movie = self.get_object(pk)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        movie = get_object_or_404(Movie, pk=pk)
+        movie = self.get_object(pk)
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -51,6 +59,6 @@ class MovieDetailView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        movie = get_object_or_404(Movie, pk=pk)
+        movie = self.get_object(pk)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
