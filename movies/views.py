d270ad022604d1cf2b8fedcdaebcd7f182105a05
arasopraza <aras.opraza@gmail.com>
2025-06-08 20:55:25 +0700

    refactor: move minioClient to function get_minio_client

--- movies/views.py
+++ movies/views.py
@@ -16,12 +16,14 @@ from minio import Minio
 from django.core.cache import cache
 from loguru import logger
 
-minioClient = Minio(
-endpoint=os.getenv('MINIO_ENDPOINT_URL'),
-    access_key=os.getenv('MINIO_ACCESS_KEY'),
-    secret_key=os.getenv('MINIO_SECRET_KEY'),
-    secure= False
-)
+def get_minio_client():
+    return Minio(
+        endpoint=os.getenv('MINIO_ENDPOINT_URL'),
+        access_key=os.getenv('MINIO_ACCESS_KEY'),
+        secret_key=os.getenv('MINIO_SECRET_KEY'),
+        secure=False
+    )
+
 
 bucket_name = os.getenv('MINIO_BUCKET_NAME')
 CACHE_KEY_LIST = "movie_list"
@@ -131,7 +133,8 @@ class MovieUploadView(APIView):
 
             try:
                 object_name = f"{serializer.instance.image.name}"
-                minioClient.fput_object(bucket_name, object_name, temp_file_path, content_type=file.content_type)
+                client = get_minio_client()
+                client.fput_object(bucket_name, object_name, temp_file_path, content_type=file.content_type)
             except Exception as e:
                 logger.error(f"Upload to Minio failed: {str(e)}")
                 return Response(
@@ -153,7 +156,8 @@ class MovieImageView(APIView):
 
         serialized_images = []
         for image in images:
-            presigned_url = minioClient.presigned_get_object(
+            client = get_minio_client()
+            presigned_url = client.presigned_get_object(
                 bucket_name,
                 image.image.name,
                 response_headers={"response-content-type": "image/jpeg"}
